67b95b50dfd133c5a2eff52568b48f172fa4a4ef
evrengokduman <sukruevrengokduman@gmail.com>
2023-06-19 15:04:12 +0300

    added functionality

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 const tagElements = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim())
+
+  tagElements.innerHTML = ''
+
+  tags.forEach((tag) => {
+    const tagElements = document.createElement('span')
+    tagElements.classList.add('tag')
+    tagElements.innerText = tag
+    tagElements.appendChild(tagElements)
+  })
+}
