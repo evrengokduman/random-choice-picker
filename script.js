2ef0a6c15256711d89988d24a04494f67609c4ca
evrengokduman <sukruevrengokduman@gmail.com>
2023-06-20 12:21:30 +0300

    added random picking function

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
-
-  if (e.key == 'Enter') {
-    setTimeput(() => {
+  if (e.key === 'Enter') {
+    setTimeout(() => {
       e.target.value = ''
     }, 10)
+
     randomSelect()
   }
 })
@@ -34,9 +34,34 @@ function randomSelect() {
   const times = 30
   const interval = setInterval(() => {
     const randomTag = pickRandomTag()
+    if (randomTag !== undefined) {
+      highlightTag(randomTag)
+
+      setTimeout(() => {
+        unHighlightTag(randomTag)
+      }, 100)
+    }
   }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
 }
 
 function pickRandomTag() {
   const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
